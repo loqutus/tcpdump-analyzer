26c2aede7b59be660b7c46ba73ae73eca2ff2aa4
rgustomi <rgustomi@localhost.localdomain>
2018-08-05 00:34:00 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3-alpine
+COPY parse.py /
+WORKDIR /
+RUN parse.py A
---
+++ parse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+import json
+request_types = {}
+domains = {}
+try:
+    sys.argv[1]
+except IndexError:
+    input_request_type = 'A'
+else:
+    input_request_type = sys.args[1]
+with open('dump.txt') as f:
+    for line in f:
+        if '>' in line:
+            split = line.split()
+            if 'domain' in split[2]:
+                request_type = split[4][:-1]
+                domain = split[5][:-1]
+                if request_type not in request_types:
+                    request_types[request_type] = 1
+                else:
+                    request_types[request_type] += 1
+                if domain not in domains:
+                    domains[domain] = 1
+                else:
+                    domains[domain] += 1
+with open('output.txt', 'wb') as f:
+    f.write(json.dumps(request_types).encode('utf-8'))
+    f.write(json.dumps(domains).encode('utf-8'))
